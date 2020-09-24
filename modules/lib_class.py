ac4a6e227f40f2ef759a11b82caac092487231ba
Joel Lundin <joel.lundin@gmail.com>
2020-09-24 09:05:23 +0200

    Removed Panda dependancy, Added Sort function

--- modules/lib_class.py
+++ modules/lib_class.py
@@ -1,5 +1,5 @@
 from modules.mediatype import Book, Movie, Music_CD
-from pandas import DataFrame
+from modules.indexing import indexing
 
 
 class Lib():
@@ -10,75 +10,55 @@ class Lib():
 
     def add_book(self, title, author, page_count, purchase_price, purchase_year):
         'Add a book to the library'
-        self.books.append(Book(title, author, page_count, purchase_price, purchase_year))
+        self.books.append(Book(title, author, int(page_count), float(purchase_price), int(purchase_year)))
+        self.update_current_value('book')
 
     def add_movie(self, title, director, length, purchase_price, purchase_year, degree_of_wear):
-        self.movies.append(Movie(title, director, length, purchase_price, purchase_year, degree_of_wear))
+        'Add a movie to the library'
+        self.movies.append(Movie(title, director, int(length), float(purchase_price), int(purchase_year), int(degree_of_wear)))
+        self.update_current_value('movie')
 
     def add_cd(self, title, artist, track_count, length, purchase_price):
-        self.music_cds.append(Music_CD(title, artist, track_count, length, purchase_price))
+        'Add a music cd to the library'
+        self.music_cds.append(Music_CD(title, artist, int(track_count), int(length), float(purchase_price)))
+        self.update_current_value('cd')
 
     # FIXME: Do not loop everything. Only that which is needed. arg eg (self, obj)
-    def update_current_value(self):
-        for obj in self.books:
-            obj.set_current_value()
-        for obj in self.movies:
-            obj.set_current_value()
-        for obj in self.music_cds:
-            obj.set_current_value(len(self.music_cds))
+    def update_current_value(self, obj):
+        'Updates the current value of an object'
+        if obj == 'book':
+            for obj in self.books:
+                obj.set_current_value()
+        elif obj == 'movie':
+            for obj in self.movies:
+                obj.set_current_value()
+        elif obj == 'cd':
+            for obj in self.music_cds:
+                # FIXME: The amount is not calculated correct. It should be similar cds not all cds.
+                obj.set_current_value(len(self.music_cds))
 
-    # FIXME: Do NOT use pandas
-    def print_lib(self):
-        print('\nBooks:')
-        temp = []
+    def show_all(self, order='title'):
+        'This will show all items in library. Default sort is by title. Argument price to sort by current value'
+        unsorted_list = []
         for book in self.books:
-            temp.append((book.title,
-                        book.author,
-                        book.page_count,
-                        book.purchase_price,
-                        book.current_value,
-                        book.purchase_year))
-        df = DataFrame(temp, columns=['Title',
-                                      'Author',
-                                      'Page Count',
-                                      'Purchase Price',
-                                      'Current Value',
-                                      'Purchase Year'])
-        df = df.set_index('Title')
-        print(df)
-        print('\nMovies:')
-        temp = []
+            unsorted_list.append(book.get_all())
+        sorted_list = indexing(unsorted_list, order)
+        print('Books:')
+        for book in sorted_list:
+            print(book)
+        unsorted_list = []
         for movie in self.movies:
-            temp.append((movie.title,
-                        movie.director,
-                        movie.length,
-                        movie.purchase_price,
-                        movie.current_value,
-                        movie.purchase_year,
-                        movie.degree_of_wear))
-        df = DataFrame(temp, columns=['Title',
-                                      'Director',
-                                      'Length',
-                                      'Purchase Price',
-                                      'Current Value',
-                                      'Purchase Year',
-                                      'Degree of wear'])
-        df = df.set_index('Title')
-        print(df)
-        print('\nMusic CDs:')
-        temp = []
+            unsorted_list.append(movie.get_all())
+        sorted_list = indexing(unsorted_list, order)
+        print('Movies:')
+        for movie in sorted_list:
+            print(movie)
+        unsorted_list = []
         for cd in self.music_cds:
-            temp.append((cd.title,
-                        cd.artist,
-                        cd.track_count,
-                        cd.length,
-                        cd.purchase_price,
-                        cd.current_value))
-        df = DataFrame(temp, columns=['Title',
-                                      'Artist',
-                                      'Track Count',
-                                      'Length',
-                                      'Purchase Price',
-                                      'Current Value'])
-        df = df.set_index('Title')
-        print(df)
+            unsorted_list.append(cd.get_all())
+        sorted_list = indexing(unsorted_list, order)
+        print('Music CDs:')
+        for cd in sorted_list:
+            print(cd)
+            # print(f'Title: {book.get_all()[0]}, Purchase Price: {book.get_all()[3]}, Current Value: {book.get_all()[5]},
+            # Author: {book.get_all()[1]}, Page Count: {book.get_all()[2]}, Purchase Year: {book.get_all()[4]}')
