882f16ec7a84174546610e8fb8ad886741c6c19c
Joel Lundin <joel.lundin@gmail.com>
2020-09-24 09:33:45 +0200

    Add a docstring to Book. Update the rest using book as template

--- modules/mediatype.py
+++ modules/mediatype.py
@@ -2,7 +2,48 @@ import modules.value as value
 
 
 class Book():
+    """
+    A class used to represent a Book
+
+    Attributes
+    ----------
+    title : str
+        the title of the book
+    author : str
+        the author of the book
+    page_count : int
+        the number of pages in the book
+    purchase_price : float
+        the purchase price of the book
+    purchase_year : int
+        the year the book was bought
+    current_value : float
+        the current value(price) of the book based on age
+
+    Methods
+    -------
+    get_all()
+        returns all attributes as a list
+    set_current_value()
+        this updates the current_value
+    """
     def __init__(self, title, author, page_count, purchase_price, purchase_year):
+        """
+        Parameters
+        ----------
+        title : str
+            the title of the book
+        author : str
+            the author of the book
+        page_count : int
+            the number of pages in the book
+        purchase_price : float
+            the purchase price of the book
+        purchase_year : int
+            the year the book was bought
+        current_value : float
+            the current value(price) of the book based on age
+        """
         self.title = title
         self.author = author
         self.page_count = page_count
@@ -11,9 +52,11 @@ class Book():
         self.current_value = 0
 
     def get_all(self):
+        'Returns a list of all attributes this class has'
         return [self.title, self.author, self.page_count, self.purchase_price, self.purchase_year, self.current_value]
 
     def set_current_value(self):
+        'This updates the current value of the instance.'
         self.current_value = value.book_value(self.purchase_price, self.purchase_year)
 
 
