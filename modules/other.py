6ca7c9b24eb16bfae50efeeb116c18283e4d58e2
Joel Lundin <joel.lundin@gmail.com>
2020-09-24 09:01:38 +0200

    Fixed some code. Add read from csv

--- modules/other.py
+++ modules/other.py
@@ -1,7 +1,12 @@
 from pprint import pprint
 import json
+import csv
 
 
+FILENAME = 'data/data.json'
+
+
+# FIXME: This should be removed if there is nothing else to do with it
 def write_to_file(data):
     keys = list(data.__dict__.keys())
     big_list = []
@@ -27,12 +32,19 @@ def write_to_json(data):
             temp_list.append(object.__dict__)
         temp_dict[key] = temp_list
 
-    with open('data/test.json', 'w') as f:
+    with open(FILENAME, 'w') as f:
         json.dump(temp_dict, f, indent=4, sort_keys=True)
 
 
 def read_from_file():
-    with open('data/test.json', 'r') as f:
+    with open(FILENAME, 'r') as f:
         import_dict = json.load(f)
 
     return import_dict
+
+
+def read_from_custom_file(filename):
+    with open(filename, 'r') as f:
+        csv_data = csv.reader(f)
+        new_list = [row for row in csv_data]
+        print(new_list)
