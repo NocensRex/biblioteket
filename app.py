d74e6ee20eea478736eac03238659c48accd95c9
Joel Lundin <joel.lundin@gmail.com>
2020-09-29 12:34:01 +0200

    Updated TODO list

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import modules.__main__ as core
 
 # Part 2
 # DONE: Save all data to a file when program closes
-# FIXME: When program starts check if a file with data exists and load it
+# DONE: When program starts check if a file with data exists and load it
 
 # Part 3
 # DONE: Make it possible to sort data in different ways
 
 # Part 4
-# TODO: OOP, Inheritance, Media
+# DONE?: OOP, Inheritance, Media
 # TODO: Docstring for everything
 # TODO: README.md
-# TODO: Import from csv
+# DONE: Import from csv
 # TODO: Manipulate objects (Update, Remove etc)
 # TODO: Try/Except on input
 
